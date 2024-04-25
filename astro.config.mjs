f0fc78790287f117445ae5fbf3e271f931784072
Carlos Lázaro <charlylionheart@gmail.com>
2024-04-25 02:31:27 -0500

    Added Theme Icon and some astro islands. Finished course aswell.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://benijs-blog.netlify.app/"
+  site: "https://benijs-blog.netlify.app/",
+  integrations: [preact()]
 });
